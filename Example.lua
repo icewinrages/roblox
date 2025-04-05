4e9f6e66a72cb4dc0fcbf5fcd85db6ae2d4692f8
icewinrages <nurelm.90@gmail.com>
2025-04-05 16:37:41 +0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,21 +1,19 @@
 local Material = loadstring(game:HttpGet("https://github.com/icewinrages/roblox/blob/master/Module.lua"), "MaterialLua")()
-local Lighting = game:GetService("Lighting")
-local RunService = game:GetService("RunService")
+local Lightning = game:GetService("Lighting")
 
 local ui = Material.Load({
-    Title = "Mining Inc Xray",
-    Style = 3,
-    SizeX = 300,
-    SizeY = 300,
-    Theme = "Dark"
+     Title = "Mining Inc Xray",
+     Style = 3,
+     SizeX = 300,
+     SizeY = 300,
+     Theme = "Dark"
 })
 
 local main = ui.New({
-    Title = "Main"
+     Title = "Main"
 })
 
 local XrayOutLineColor = Color3.new(0, 0, 0)
-
 main.ColorPicker({
     Text = "XRay Outline Color",
     Callback = function(color)
@@ -24,16 +22,17 @@ main.ColorPicker({
     Default = XrayOutLineColor
 })
 
-local function getAllOre()
+function getAllOre()
     local ores = {}
-    for _, v in ipairs(game:GetService("Workspace").Minerals:GetChildren()) do
+    for i,v in ipairs(game:GetService("Workspace").Minerals:GetChildren()) do
+        local model = v
         local oreName = v.Type.Value
-        table.insert(ores, {model = v, oreName = oreName})
+        ores[#ores+1] = {model = model, oreName = oreName}
     end
     return ores
 end
 
-local function drawOreEsp(ore)
+function drawOreEsp(ore)
     local billboardGui = Instance.new("BillboardGui")
     billboardGui.Name = "OreEsp"
     billboardGui.Adornee = ore.model
@@ -41,7 +40,6 @@ local function drawOreEsp(ore)
     billboardGui.Size = UDim2.new(0, 100, 0, 100)
     billboardGui.StudsOffset = Vector3.new(0, 2, 0)
     billboardGui.Parent = ore.model
-
     local esp = Instance.new("TextLabel", billboardGui)
     esp.Name = "Text"
     esp.Size = UDim2.new(0, 100, 0, 10)
@@ -55,22 +53,14 @@ local function drawOreEsp(ore)
     esp.TextXAlignment = Enum.TextXAlignment.Center
     esp.TextYAlignment = Enum.TextYAlignment.Center
     esp.TextSize = 10
-
     print("Drawed Esp", "Ore Name: " .. ore.oreName)
 end
 
-local function deleteOreEsp()
-    for _, v in ipairs(workspace.Minerals:GetChildren()) do
+function deleteOreEsp()
+    for i,v in ipairs(workspace.Minerals:GetChildren()) do
         if v:FindFirstChild("OreEsp") then
             v.OreEsp:Destroy()
         end
-    end
-end
-
-local function updateOreEsp()
-    deleteOreEsp()
-    for _, v in ipairs(getAllOre()) do
-        drawOreEsp(v)
     end
 end
 
@@ -78,11 +68,8 @@ main.Toggle({
     Text = "Toggle Xray",
     Callback = function(value)
         if value then
-            updateOreEsp()
-            -- Обновление каждые 60 секунд
-            while main:GetToggle("Toggle Xray") do
-                updateOreEsp()
-                wait(60)
+            for i,v in ipairs(getAllOre()) do
+                drawOreEsp(v)
             end
         else
             deleteOreEsp()
@@ -92,21 +79,15 @@ main.Toggle({
 
 main.Toggle({
     Text = "Fullbright",
-    Callback = function(value)
+    Callback = function (value)
         if value then 
-            Lighting.Ambient = Color3.new(1, 1, 1)
-            Lighting.ColorShift_Bottom = Color3.new(1, 1, 1)
-            Lighting.ColorShift_Top = Color3.new(1, 1, 1)
+            Lightning.Ambient = Color3.new(1, 1, 1)
+            Lightning.ColorShift_Bottom = Color3.new(1, 1, 1)
+            Lightning.ColorShift_Top = Color3.new(1, 1, 1)
         else
-            Lighting.Ambient = Color3.new(0.5, 0.5, 0.5)
-            Lighting.ColorShift_Bottom = Color3.new(0.5, 0.5, 0.5)
-            Lighting.ColorShift_Top = Color3.new(0.5, 0.5, 0.5)
+            Lightning.Ambient = Color3.new(0.5, 0.5, 0.5)
+            Lightning.ColorShift_Bottom = Color3.new(0.5, 0.5, 0.5)
+            Lightning.ColorShift_Top = Color3.new(0.5, 0.5, 0.5)
         end
     end
 })
-
--- Поддержка мобильных устройств
-if game:GetService("User InputService").TouchEnabled then
-    ui.SizeX = 200
-    ui.SizeY = 200
-end
